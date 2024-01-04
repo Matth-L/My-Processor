668006dcffa28e12d199b38e5dbc365b185180e3
Matthias Lapu <matthias.lapu@ensiie.eu>
2024-01-04 16:59:02 +0100

    3.8 ne fonctionne pas encore

---
+++ test/ex3_8.asm
@@ -0,0 +1,28 @@
+start: 
+  in r0 # int n
+  in r1 # char c 
+  ldi r3, 0 # i = 0 
+  jmp reset_j # on init j 
+
+reset_j: 
+  ldi r4, 0 # reset j 
+  jmp loop_i # on lance la boucle
+
+input_enter:
+  ldi r2, 13
+  out r2
+  jmp reset_j
+
+loop_j:
+  addi r4, r4, 1 # j++
+  out r1 #print c 
+  jeq r4, r0, input_enter # on passe à entrée et on termine j 
+  jmp loop_j
+
+loop_i:
+  addi r3, r3, 1 # i++
+  jeq r3, r0, end # i = n ? 
+  jmp loop_j
+  
+end: 
+  jmp end
